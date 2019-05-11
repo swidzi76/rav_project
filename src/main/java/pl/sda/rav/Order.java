5deb7ea0c004bbe4bd12f91d9d6219aa276e9c06
Jarek <swidzi76@gmail.com>
2019-05-11 10:36:43 +0200

    make class Order

---
+++ src/main/java/pl/sda/rav/Order.java
@@ -0,0 +1,6 @@
+package pl.sda.rav;
+
+public class Order {
+    private int id;
+    private User customer;
+}
--- src/main/java/pl/sda/rav/User.java
+++ src/main/java/pl/sda/rav/User.java
@@ -5,5 +5,33 @@ public class User {
     private String password;
     private UserType userType;
 
+    public User(String login, String password, UserType userType) {
+        this.login = login;
+        this.password = password;
+        this.userType = userType;
+    }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
 }
