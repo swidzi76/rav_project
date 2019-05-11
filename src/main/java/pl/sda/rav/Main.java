8b70b6086b88880bf620e515fb314fcc897c94e1
Jarek <swidzi76@gmail.com>
2019-05-11 11:33:29 +0200

    make class Order

--- src/main/java/pl/sda/rav/Main.java
+++ src/main/java/pl/sda/rav/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(" TEST");
         User user = new User("Admin","admin",UserType.ADMINITRATOR);
-        Car car = new Car(12,"sam",2018,300);
+        Car car = new Car(12,"sam",2017,300);
 
     }
 }
